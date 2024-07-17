a59c13376822845824a41370e535805437f66ad7
Irshad <irshadm@alokin.in>
2024-07-17 16:33:35 +0530

    windows support removed

--- test/file_picker_windows_test.dart
+++ test/file_picker_windows_test.dart
@@ -5,8 +5,6 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:custom_file_picker/src/exceptions.dart';
 import 'package:custom_file_picker/src/file_picker.dart';
-import 'package:custom_file_picker/src/windows/file_picker_windows.dart';
-import 'package:custom_file_picker/src/windows/file_picker_windows_ffi_types.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
